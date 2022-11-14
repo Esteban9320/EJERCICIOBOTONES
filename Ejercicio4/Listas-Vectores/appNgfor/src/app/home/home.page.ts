1de7f3585b09b6f452450c3095155a4fafa3293a
gustavo <gustavoest1393@gmail.com>
2022-11-14 16:56:08 -0500

    first commit

---
+++ Ejercicio4/Listas-Vectores/appNgfor/src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  marvelList: any[];
+    constructor() {
+      this.marvelList = [
+        {
+          name:'Iron Man',
+          color: 'red',
+        },
+        {
+          name:'Capitan America',
+          color: 'blue',
+        },
+        {
+          name:'Hulk',
+          color: 'green',
+        }
+      ];
+    }
+
+}
